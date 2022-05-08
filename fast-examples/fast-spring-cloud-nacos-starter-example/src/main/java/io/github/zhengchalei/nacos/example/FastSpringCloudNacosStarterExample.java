1a67bca3d7db2bc5dcab84afdc90eb5b5a0d3efd
zhengchalei <stone981023@gmail.com>
2022-05-09 01:10:17 +0800

    ✨:  1.0.3.3 修复负载均衡错误, 以及添加测试案例
    Submit Branch: master

--- fast-examples/fast-spring-cloud-nacos-starter-example/src/main/java/io/github/zhengchalei/nacos/example/FastSpringCloudNacosStarterExample.java
+++ fast-examples/fast-spring-cloud-nacos-starter-example/src/main/java/io/github/zhengchalei/nacos/example/FastSpringCloudNacosStarterExample.java
@@ -1,12 +1,23 @@
 package io.github.zhengchalei.nacos.example;
 
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FastSpringCloudNacosStarterExample {
     public static void main(String[] args) {
         SpringApplication.run(FastSpringCloudNacosStarterExample.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(RestTemplate restTemplate) {
+        return args -> {
+//            String json = restTemplate.getForObject("http://service/test", String.class);
+//            System.out.println(json);
+        };
     }
 }
--- fast-starters/fast-spring-cloud-nacos-starter/src/main/java/io/github/zhengchalei/nacos/RestTemplateConfig.java
+++ fast-starters/fast-spring-cloud-nacos-starter/src/main/java/io/github/zhengchalei/nacos/RestTemplateConfig.java
@@ -5,26 +5,19 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 /**
  * @author <a href="mailto:stone981023@gmail.com">zhengchalei</a>
  **/
-@Component
 @EnableFeignClients
 @ConditionalOnMissingBean(RestTemplate.class)
 public class RestTemplateConfig {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(List<ClientHttpRequestInterceptor> clientHttpRequestInterceptors) {
-        return new RestTemplateBuilder()
-                .interceptors(clientHttpRequestInterceptors)
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
     }
 
 }
